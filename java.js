0898d99298575d21220670fc8e70dd7bad0af990
Manaskannikanti <149857102+Manaskannikanti@users.noreply.github.com>
2026-01-28 09:59:14 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,34 @@
+// Selecting elements
+let menuIcon = document.querySelector('.menu-icon');
+let navbar = document.querySelector('.navbar');
+let sections = document.querySelectorAll('section');
+let navlinks = document.querySelectorAll('header nav a');
+
+// Toggle menu on hamburger click
+menuIcon.onclick = () => {
+    navbar.classList.toggle('active');
+};
+
+// Scroll section active link
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlinks.forEach(link => {
+                link.classList.remove('active');
+            });
+
+            document
+                .querySelector('header nav a[href*=' + id + ']')
+                .classList.add('active');
+        }
+    });
+
+    // Close mobile menu on scroll
+    navbar.classList.remove('active');
+};
